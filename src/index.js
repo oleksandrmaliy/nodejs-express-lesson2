1723dfd780f02901eaddd59733ced50c942bf284
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-05-20 12:04:29 +0300

    task 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-// import { PATH_DB, q } from './constants/contacts';
-// // import path from 'node:path';
-// // const pathToWorkDir = path.join(process.cwd(), 'src', 'db', 'db.json');
-// console.log(PATH_DB);
-// console.log(q);
+import { PATH_DB } from './constants/contacts.js';
+console.log('index ' + PATH_DB);
