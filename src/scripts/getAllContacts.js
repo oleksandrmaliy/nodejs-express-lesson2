c2fd086eea1d3db898b9bae701f7f8fd50e7cb4c
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-05-26 14:52:36 +0300

    task4 done

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, 'utf8');
+
+    return JSON.parse(contacts);
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+  }
+};
 
 console.log(await getAllContacts());
