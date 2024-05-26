48c60ee31002596780ecf7ee426939944bb10c66
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-05-26 15:08:31 +0300

    task6 done

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const noContacts = JSON.stringify([]);
+    await fs.writeFile(PATH_DB, noContacts);
+
+    console.log('Дані успішно записані у файл.');
+  } catch (err) {
+    console.error('Помилка запису у файл:', err);
+  }
+};
 
 await removeAllContacts();
