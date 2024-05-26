012af01aa3e43adfb192b3de7304340c3eee6a4e
Oleksandr <115729512+oleksandrmaliy@users.noreply.github.com>
2024-05-26 17:15:04 +0300

    Completd. To send.

--- src/index.js
+++
@@ -1,4 +0,0 @@
-// import { PATH_DB } from './constants/contacts.js';
-// import { createFakeContact } from './utils/createFakeContact.js';
-// console.log('index ' + PATH_DB);
-// console.log(createFakeContact());
