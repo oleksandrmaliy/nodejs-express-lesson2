c5fcd47aaf64f78db72f2177b28657c11321c0d1
Oleksandr <avmaliy71@gmail.com>
2024-06-25 11:13:21 +0300

    ok

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -11,3 +13,13 @@ export const ROLES = {
   TEACHER: 'teacher',
   PARENT: 'parent',
 };
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
